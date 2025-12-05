a274c2d2ca2b67787337666ad6d718614d52926a
porrasAndres22 <andresporras202020@gmail.com>
2025-12-05 16:16:26 -0600

    rebuild 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -32,6 +31,5 @@ export default function RootLayout({
           {children}
         </body>
       </html>
-    </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div className="fluentContent">
+
     </div>
   );
 }
