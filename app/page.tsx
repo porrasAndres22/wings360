e525913874ae9d63232f393afbe3334458fdd2a0
porrasAndres22 <andresporras202020@gmail.com>
2025-12-05 16:07:50 -0600

    rebuild app 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,8 @@
 
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div className="fluentContent">
-      <div className="formLogin form animate__animated animate__fadeIn">
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-          <SignedOut>
-            <SignInButton />
-            <SignUpButton>
-              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </header>
-      </div>
     </div>
   );
 }
