538af46dadf4afa294f56a10b402d6d9352ca101
porrasAndres22 <andresporras202020@gmail.com>
2025-12-08 16:59:16 -0600

    add singIn Page

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,14 @@ import {
 export default function Home() {
   return (
     <div className="fluentContent">
-      <div className="formLogin form animate__animated animate__fadeIn">
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </header>
-      </div>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </div>
   );
 }
